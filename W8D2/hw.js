ef2adf392a3279ca1c031b5720a8684d089e582d
ctvidic <chrisvidic.cv@gmail.com>
2021-08-03 08:13:07 -0400

    first js

---
+++ W8D2/hw.js
@@ -0,0 +1,50 @@
+function mysteryScoping1() {
+    var x = 'out of block';
+    if (true) {
+        var x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping2() {
+    const x = 'out of block';
+    if (true) {
+        const x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping3() {
+    const x = 'out of block';
+    if (true) {
+        var x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+function mysteryScoping4() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping5() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    let x = 'out of block again';
+    console.log(x);
+}
+
+function madLib(word1,word2,word3){
+    console.log(`We shall ${word1.toUpperCase()} the ${word2.toUpperCase()} ${word3.toUpperCase()}.`);
+}
+
+function isString\
